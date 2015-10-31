69d0caccc732732ac17ad8539cc93f49e2535d76
Alexander Taranov <taranov.a@i20.biz>
2015-11-01 02:02:32 +0600

    initial

--- app/app.controller.js
+++ app/app.controller.js
@@ -2,51 +2,58 @@
 
 angular
   .module('app')
-  .controller('ChangeMode', ['$scope', function ($scope) {
+  .controller('SceneController', SceneController);
 
-    var modes = this;
+SceneController.$inject = ['$scope'];
 
-    modes.collection = [
-      {
-        "messages": ["Need some JS?", "I'm in!", "Javascript, jQuery, Angular, Ajax"],
-        "imgSrc": "images/super-man.png",
-        "nav": "Day mode",
-        "id": 0
-      },
-      {
-        "messages": ["Psssss....", "I have some magic!", "HTML 5, HTML 4, CSS3, CSS2, Sass, Compass, swig"],
-        "imgSrc": "images/super-man.png",
-        "nav": "Night mode",
-        "id": 1
-      },
-      {
-        "messages": ["Bug will not pass!!!"],
-        "imgSrc": "images/super-man.png",
-        "nav": "Siberian mode",
-        "id": 2
-      }
-    ];
+function SceneController($scope) {
 
-    $scope.activeMode = modes.collection[0];
-    $scope.devStyle = {'background-image': 'url(' + $scope.activeMode.imgSrc + ')'};
+  var modes = this,
+    windowElement = angular.element(window);
 
-    $scope.showMode = function (idx) {
-      $scope.activeMode = modes.collection[idx];
-    };
+  windowElement.load(function(){
+    angular.element('#loader').hide();
+  });
 
-    $scope.$on('showMessage', function (ngRepeatFinishedEvent) {
-      var items = angular.element('.developer__message');
+  modes.collection = [
+    {
+      "messages": ["Need some JS?", "I'm in!", "Javascript, jQuery, Angular, Ajax"],
+      "imgSrc": "images/super-man.png",
+      "nav": "Day mode",
+      "id": 0
+    },
+    {
+      "messages": ["Psssss....", "I have some magic!", "HTML 5, HTML 4, CSS3, CSS2, Sass, Compass, swig"],
+      "imgSrc": "images/super-man.png",
+      "nav": "Night mode",
+      "id": 1
+    },
+    {
+      "messages": ["Bug will not pass!!!"],
+      "imgSrc": "images/super-man.png",
+      "nav": "Siberian mode",
+      "id": 2
+    }
+  ];
+  modes.activeMode = modes.collection[0];
+  modes.devStyle = {'background-image': 'url(' + modes.activeMode.imgSrc + ')'};
 
-      showMess(items, 0);
+  modes.showMode = function (idx) {
+    modes.activeMode = modes.collection[idx];
+  };
 
-      function showMess(mess, idx) {
-        var $elements = mess;
-        $elements.eq(idx).fadeIn(2000).fadeOut(2000, function () {
-          showMess(mess, idx + 1 < $elements.length ? idx + 1 : 0);
-        });
-      }
-    });
-  }]);
+  $scope.$on('showMessage', function (ngRepeatFinishedEvent) {
+    var items = angular.element('.developer__message');
+
+    showMess(items, 0);
+    function showMess(mess, idx) {
+      var $elements = mess;
+      $elements.eq(idx).fadeIn(2000).fadeOut(2000, function () {
+        showMess(mess, idx + 1 < $elements.length ? idx + 1 : 0);
+      });
+    }
+  });
+}
 
 
 
