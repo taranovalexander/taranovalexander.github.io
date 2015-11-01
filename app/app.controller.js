48e441c9b564ed4e523402b865fe27d35d672f29
Alexander Taranov <taranov.a@i20.biz>
2015-11-01 18:43:26 +0600

    initial

--- app/app.controller.js
+++ app/app.controller.js
@@ -12,44 +12,46 @@ function SceneController($scope) {
     windowElement = angular.element(window);
 
   windowElement.load(function(){
-    angular.element('#loader').hide();
+    //angular.element('#loader').hide();
   });
 
   modes.collection = [
     {
       "messages": ["Need some JS?", "I'm in!", "Javascript, jQuery, Angular, Ajax"],
-      "imgSrc": "images/super-man.png",
-      "nav": "Day mode",
+      "imgSrc": "images/mod-1.png",
+      "nav": "Siberian mode",
       "id": 0
     },
     {
       "messages": ["Psssss....", "I have some magic!", "HTML 5, HTML 4, CSS3, CSS2, Sass, Compass, swig"],
-      "imgSrc": "images/super-man.png",
+      "imgSrc": "images/mod-2.png",
       "nav": "Night mode",
       "id": 1
     },
     {
       "messages": ["Bug will not pass!!!"],
-      "imgSrc": "images/super-man.png",
-      "nav": "Siberian mode",
+      "imgSrc": "images/mod-3.png",
+      "nav": "Middle-earth mode",
       "id": 2
     }
   ];
   modes.activeMode = modes.collection[0];
-  modes.devStyle = {'background-image': 'url(' + modes.activeMode.imgSrc + ')'};
 
   modes.showMode = function (idx) {
     modes.activeMode = modes.collection[idx];
   };
 
   $scope.$on('showMessage', function (ngRepeatFinishedEvent) {
-    showMess(0);
+    var mess = angular.element('.developer__message');
+
+    showMess(0, mess);
+    modes.devStyle = {'background-image': 'url(' + modes.activeMode.imgSrc + ')'};
   });
 }
 
-function showMess(idx) {
-  var $elements = angular.element('.developer__message');
+function showMess(idx, mess) {
+  var $elements = mess;
   $elements.eq(idx).fadeIn(2000).fadeOut(2000, function () {
-    showMess(idx + 1 < $elements.length ? idx + 1 : 0);
+    showMess(idx + 1 < $elements.length ? idx + 1 : 0, mess);
   });
 }
