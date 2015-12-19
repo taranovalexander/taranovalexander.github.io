7a6740bdaf679692dae53bc9c7cb529a13243697
Taranov Alexander <taranov.a@i20.biz>
2015-12-19 12:17:11 +0600

    added sections

---
+++ app/app.directive.js
@@ -0,0 +1,32 @@
+"use strict";
+
+angular
+    .module('app')
+    .directive('pageScroll', pageScroll);
+
+function pageScroll() {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var regions = element.find('.section, .scene');
+      angular.element(window).on('scroll', onScroll);
+
+      function onScroll() {
+        var scrollPos = angular.element(window).scrollTop(),
+            wHeight = angular.element(window).height();
+
+        angular.forEach(regions, function(value, key) {
+          var $currentElement = angular.element(value);
+          if ($currentElement.position().top < scrollPos + wHeight/2 &&
+              $currentElement.position().top +  $currentElement.height() > scrollPos + wHeight/2) {
+            if (!$currentElement.hasClass('active')){
+              regions.removeClass("active");
+              $currentElement.addClass("active");
+            }
+          }
+        });
+      }
+    }
+  }
+}
+
